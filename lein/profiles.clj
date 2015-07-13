e3d84a2947655549fe692cfed193350196c6af6d
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-07-12 22:30:48 -0400

    global lein updates
    nrepl `0.2.10`, squiggly and refactor nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "1.0.5"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                       [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]}}
