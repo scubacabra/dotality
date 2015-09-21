9fe03e4ae467a719c871f47cb03aa53805f62a09
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-09-21 12:27:26 -0400

    refactor repl version bump

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.0.5"]]
+                  [refactor-nrepl "1.1.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]}}
