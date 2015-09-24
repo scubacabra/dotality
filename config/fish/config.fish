737020f2e7d5d3b1eb9b1741d281ab5b399a8229
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-09-24 11:33:36 -0400

    new docker configs with docker-machine

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -23,8 +23,8 @@ Plugin gvm
 _prepend_path /usr/local/opt/coreutils/libexec/gnubin
 
 # AA docker variables
-set -gx DOCKER_HOST tcp://192.168.59.103:2376
-set -gx DOCKER_CERT_PATH /Users/jacobono/.boot2docker/certs/boot2docker-vm
+set -gx DOCKER_HOST tcp://192.168.99.100:2376
+set -gx DOCKER_CERT_PATH /Users/jacobono/.docker/machine/machines/dev
 set -gx DOCKER_TLS_VERIFY 1
 
 # Homebrew Cask symlink to /Applications
