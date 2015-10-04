20925ca42e87680abafbc26fb3c7953ac41fde46
Daniel Mijares <daniel.j.mijares@gmail.com>
2015-10-04 12:24:05 -0400

    DOCKER_IP env var for fish - usual docker-machine

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -24,6 +24,7 @@ _prepend_path /usr/local/opt/coreutils/libexec/gnubin
 
 # AA docker variables
 set -gx DOCKER_HOST tcp://192.168.99.100:2376
+set -gx DOCKER_IP 192.168.99.100
 set -gx DOCKER_CERT_PATH /Users/jacobono/.docker/machine/machines/dev
 set -gx DOCKER_TLS_VERIFY 1
 
